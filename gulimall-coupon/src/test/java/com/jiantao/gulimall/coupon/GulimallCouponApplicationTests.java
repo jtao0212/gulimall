2bab9f48714f32663210859fb6f444ba5926b1b3
jiantao <jtao0212@163.com>
2020-07-22 19:51:41 +0800

    init project

---
+++ gulimall-coupon/src/test/java/com/jiantao/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiantao.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
